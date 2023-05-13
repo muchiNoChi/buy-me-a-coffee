fac28e332ef83c2f60a31682b0b81d6af7f0c39e
Aliaksandra Abramovich <aliaksandra.abramovich@oliverwyman.com>
2023-05-13 23:24:20 +0200

    Add Fastify.ts server and Webpack, serve index page with Fastify-static

---
+++ main.ts
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  const connectWalletBtn: HTMLElement | null = document.getElementById('connectWalletBtn');
+
+  connectWalletBtn?.addEventListener('click', () => {
+    // Handle the wallet connection logic here
+    alert('Wallet connection functionality will be implemented here.');
+  });
+});
